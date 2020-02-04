58b52ab73321363a8b88b11bf8c4af39308e06dd
liyou <1014041610@qq.com>
2020-02-04 19:35:40 +0800

    delete idea file

--- src/com/leetcode/TwoSum.java
+++ src/com/leetcode/TwoSum.java
@@ -1,11 +1,18 @@
-package leetcode;
+package com.leetcode;
 
 /**
  * Created by You.Li
  */
 public class TwoSum {
     public static void main(String[] args) {
-        
+        int arry[]={1,3,6,7,9};
+
+
+
+    }
+
+    public int[] getIndex(int[] arr,int target){
+
     }
 
 }
