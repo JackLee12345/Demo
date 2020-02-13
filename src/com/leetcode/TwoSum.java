9248128e68083c4d8c2328e2a9d09704e21d0eec
liyou <1014041610@qq.com>
2020-02-13 14:45:35 +0800

    nn

--- src/com/leetcode/TwoSum.java
+++ src/com/leetcode/TwoSum.java
@@ -1,17 +1,12 @@
-package com.leetcode;
+package leetcode;
 
 /**
  * Created by You.Li
  */
 public class TwoSum {
     public static void main(String[] args) {
-        int arry[]={1,3,6,7,9};
-
-
-
-    }
-
-    public int[] getIndex(int[] arr,int target){
+        int[] arr = new int[]{2,7,11,19};
+        int total = 9;
 
     }
 
