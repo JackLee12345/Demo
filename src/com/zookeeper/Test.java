fdb401080038052ab8b3a06bfd02dddd11a6d5eb
liyou <1014041610@qq.com>
2020-02-20 00:16:39 +0800

    ooo

--- src/com/zookeeper/Test.java
+++ src/com/zookeeper/Test.java
@@ -4,8 +4,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("haha");
         System.out.println();
-        System.out.println("one");
-        System.out.println("111");
-        System.out.println("222");
     }
 }
